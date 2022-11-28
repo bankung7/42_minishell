4c7c12cacb0c06a5bae9b12ede69242439d325bf
SoundPJ <57010936@kmitl.ac.th>
2022-11-28 12:05:09 +0700

    ..

--- srcs/execute.c
+++ srcs/execute.c
@@ -52,59 +52,33 @@ char *ft_iscmd(char *str)
 int ft_runcmd(t_mini *data)
 {
 	// int		fd[2];
-	// pid_t	pid;
+	pid_t	pid;
 	t_cmd	*head;
 
-	// if (!data->cmdlist)
-	// 	return (-1);
-	// if (pipe(fd) < 0)
-	// {
-	// 	ft_putstr_fd("Error at pipe.", 2);
-	// 	return (-1);
-	// }
+	if (!data->cmdlist)
+		return (-1);
 	head = data->cmdlist;
-	if (head->next != NULL)
-		ft_pipex(head);
-	// if (head->infile != 0)
-	// 	dup2(head->infile, 0);
-	// while (head)
-	// {
-	// 	if (head != data->cmdlist)
-	// 		dup2(fd[0], 0);
-	// 	pid = fork();
-	// 	if (pid == -1)
-	// 		return (-1);
-	// 	else if (pid == 0)
-	// 	{
-    //         close(fd[0]);
-	// 		dprintf(2, "next = %p\n", head->next);
-	// 		if (head->next != NULL)
-	// 		{
-	// 			dprintf(2, "INN\n");
-	// 			dup2(fd[1], 1);
-	// 		}
-	// 		close(fd[1]);
-	// 		if (head->type == BUILTIN)
-    //             ft_isbuiltin(head, 1);
-    //         else
-    //         {
-    //             char	*bin_path = ft_iscmd(head->path);
-	// 			execve(bin_path, head->argv, 0);
-    //             free(bin_path);
-    //         }
-	// 	}
-	// 	else
-	// 	{
-	// 		wait(NULL);
-	// 		dprintf(2, "HEREEE\n");
-	// 		// close(fd[1]);
-	// 		dup2(fd[0], 0);
-	// 		// close(fd[0]);
-	// 	}
-	// 	head = head->next;
-	// 	dprintf(2, "head = %p\n", head);
-	// }
-	// printf("OUT\n");
+	// if (head->next != NULL)
+	// 	ft_pipex(head);
+	while (head)
+	{
+		pid = fork();
+		if (pid == -1)
+			return (-1);
+		else if (pid == 0)
+		{
+			if (head->next == NULL)
+				execve(ft_iscmd(head->path), head->argv, 0);
+			else
+			{
+				ft_pipex(head);
+				break ;
+			}
+		}
+		else
+			wait(NULL);
+		head = head->next;
+	}
 	return (0);
 }
 
@@ -112,12 +86,15 @@ int	ft_pipex(t_cmd *head)
 {
 	pid_t	pid;
 	int		fd[2];
+	(void)	head;
 
+	printf("In pipex...\n");
 	if (pipe(fd) != 0)
 	{
 		ft_putstr_fd("Error at pipe.", 2);
 		return (-1);
 	}
+	printf("Done piping..\n");
 	pid = fork();
 	if (pid < 0)
 		return (-1);
@@ -132,19 +109,19 @@ int	ft_pipex(t_cmd *head)
 	else
 	{
 		wait(NULL);
-		close(fd[1]);
-		dup2(fd[0], 0);
-		// if (++i < ac - 2)
-		if (head->next != NULL)
-		{
-			head = head->next;
-			printf("HERERE\n");
-			ft_pipex(head);
-		}
-		else
-			return (0);
-		// 	execve(ft_iscmd(head->path), head->argv, 0);
-			// runcmd(paths, av, open(av[ac - 1], O_WRONLY | O_CREAT, 0775), i);
+		// close(fd[1]);
+	// 	dup2(fd[0], 0);
+	// 	// if (++i < ac - 2)
+	// 	if (head->next != NULL)
+	// 	{
+	// 		head = head->next;
+	// 		printf("HERERE\n");
+	// 		ft_pipex(head);
+	// 	}
+	// 	else
+	// 		return (0);
+	// 	// 	execve(ft_iscmd(head->path), head->argv, 0);
+	// 		// runcmd(paths, av, open(av[ac - 1], O_WRONLY | O_CREAT, 0775), i);
 	}
 	return (0);
 }
